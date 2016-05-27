15930df8fbfc99cb9ee67a5713b96875e99638ca
jouyouyun <jouyouwen717@gmail.com>
2016-05-27 11:12:44 +0800

    mounts: Fix mount volume failed
    Change-Id: I4a9433b68f598918a0f458ca3c6e3d647d1b6955

--- mounts/monitor.go
+++ mounts/monitor.go
@@ -25,16 +25,19 @@ func (m *Manager) handleEvent() {
 		info := newDiskInfoFromVolume(volume)
 		logger.Debug("[Event] volume added:", info.Name, info.Type)
 		if info.Type == DiskTypeRemovable && m.isAutoMount() {
-			m.mountVolume(info.Id, info.object)
+			m.mountVolume(info.Id, volume)
+			volume.Unref()
 			return
 		}
-		m.setPropDiskList(m.DiskList.add(info))
+		volume.Unref()
+		m.refrashDiskList()
 		dbus.Emit(m, "Changed", EventTypeVolumeAdded, info.Id)
 	})
 
 	m.monitor.Connect("volume-removed", func(monitor *gio.VolumeMonitor,
 		volume *gio.Volume) {
 		logger.Debug("[Event] volume removed")
+		volume.Unref()
 		soundutils.PlaySystemSound(soundutils.EventDeviceUnplug,
 			"", false)
 		oldInfos := m.DiskList.duplicate()
@@ -49,13 +52,14 @@ func (m *Manager) handleEvent() {
 	m.monitor.Connect("mount-added", func(monitor *gio.VolumeMonitor,
 		mount *gio.Mount) {
 		info := newDiskInfoFromMount(mount)
+		mount.Unref()
 		logger.Debug("[Event] mount added:", info.Name, info.CanEject)
 		if info.CanEject && m.isAutoOpen() {
 			go exec.Command("/bin/sh", "-c",
 				fmt.Sprintf("gvfs-open %s",
 					info.MountPoint)).Run()
 		}
-		m.setPropDiskList(m.DiskList.add(info))
+		m.refrashDiskList()
 		dbus.Emit(m, "Changed", EventTypeMountAdded, info.Id)
 	})
 
@@ -71,6 +75,7 @@ func (m *Manager) handleEvent() {
 		point := root.GetUri()
 		info, err := m.DiskList.getByMountPoint(point)
 		root.Unref()
+		mount.Unref()
 		if err != nil {
 			logger.Warning(err)
 			return
